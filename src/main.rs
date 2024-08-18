9eb992da5b8f4e2274064ef4a9cd274b78c749b0
rowan amber-jones <roarch@proton.me>
2024-08-18 08:47:25 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use chrono::{DateTime, NaiveDateTime};
+use icalendar::{Calendar, Event};
+use std::io::Error;
+
+//fn get_calendar_events(cal: &Calendar) -> Result<Vec<Event>, io::Error> {}
+fn main() {
+    let foo = env!("foo");
+    println!("{}", foo);
+    println!("Hello, world!");
+}
